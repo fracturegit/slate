d8e64fe2c4d88c82286b69f5089a6d3987973838
Daniel Orr <orrdaniel73@gmail.com>
2026-02-05 23:13:34 +0000

    feat: gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,57 @@
-@file:Suppress("PropertyName")
+plugins {
+    kotlin("jvm") version("2.3.0")
+    id("java")
+    id("maven-publish")
+}
 
-val minestom_version: String by properties
+group = "net.mcbrawls"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+    maven("https://maven.enginehub.org/repo/")
+    maven("https://maven.noxcrew.com/public/")
+}
 
 dependencies {
-    implementation("net.minestom:minestom:$minestom_version")
+    api("net.minestom:minestom:2026.01.08-1.21.11")
     api("net.kyori:adventure-api:4.25.0")
     api("com.noxcrew.noxesium:api:3.0.0")
+    api("org.apache.commons:commons-lang3:3.20.0")
+    api("it.unimi.dsi:fastutil:8.5.12")
+    api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+}
+
+kotlin {
+    jvmToolchain(25)
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+            groupId = project.group as String
+            artifactId = project.name
+            version = project.version as String
+        }
+    }
+
+    repositories {
+        val mavenUrl = System.getenv("MAVEN_URL")
+        if (mavenUrl != null) {
+            maven {
+                name = "envmaven"
+                url = uri(mavenUrl)
+                credentials {
+                    username = System.getenv("MAVEN_USERNAME")
+                    password = System.getenv("MAVEN_PASSWORD")
+                }
+            }
+        }
+    }
 }
